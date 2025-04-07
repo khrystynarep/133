f2eff5a4778146d138b48e0a6ac66e636b064b72
Nikita <86921169+MasterNikPro@users.noreply.github.com>
2025-04-07 09:53:42 +0300

    Updated create files logic
    updated two files create file and log.py logic, to create only one file like a log format

--- task3/create_files.sh
+++ task3/create_files.sh
@@ -4,21 +4,15 @@ HOSTNAME=$(hostname)
 DATE_TIME=$(date +"%Y-%m-%d %H:%M:%S")
 
 case $HOSTNAME in
-  sftp1)
-    NEIGHBORS=("192.168.56.12" "192.168.56.13")
-    ;;
-  sftp2)
-    NEIGHBORS=("192.168.56.11" "192.168.56.13")
-    ;;
-  sftp3)
-    NEIGHBORS=("192.168.56.11" "192.168.56.12")
-    ;;
+  sftp1) NEIGHBORS=("192.168.56.12" "192.168.56.13") ;;
+  sftp2) NEIGHBORS=("192.168.56.11" "192.168.56.13") ;;
+  sftp3) NEIGHBORS=("192.168.56.11" "192.168.56.12") ;;
 esac
 
 for N in "${NEIGHBORS[@]}"; do
-  FILENAME="created_by_${HOSTNAME}_at_$(date +'%Y%m%d_%H%M%S').txt"
-  CONTENT="Created by: $HOSTNAME
-DateTime: $DATE_TIME
-"
-  echo "$CONTENT" | ssh -o StrictHostKeyChecking=no -i /home/sftpuser/.ssh/id_rsa sftpuser@$N "cat > /home/sftpuser/$FILENAME"
+  TARGET_HOST=$(ssh -i /home/sftpuser/.ssh/id_rsa -o StrictHostKeyChecking=no sftpuser@$N "hostname")
+  LOG_ENTRY="$DATE_TIME | Created connection from $HOSTNAME to $TARGET_HOST"
+
+  echo "$LOG_ENTRY" | ssh -i /home/sftpuser/.ssh/id_rsa -o StrictHostKeyChecking=no sftpuser@$N \
+    "echo \"$LOG_ENTRY\" >> /home/sftpuser/connection_log.txt"
 done
